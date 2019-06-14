9672c776fbe2319b7158a1820f8ea1d93ffa3203
Yulya Vorontsova <secondnumber01@gmail.com>
2019-06-14 13:29:06 +0300

    friends list

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,29 +1,24 @@
 'use strict';
 
-/**
- * Генерация HTML списка друзей
- * @param {Friend[]} friends
- * @return {HTMLUListElement}
- */
 
-let friends = [
-    {
-        firstName: 'Artsiom',
-        lastName: 'Mezin'
-    },
-    {
-        firstName: 'Ilia',
-        lastName: 'Kantor'
-    },
-    {
-        firstName: 'Christopher',
-        lastName: 'Michael'
-    }
-];
+function makeFriendsList(data) {
+    let dataNew = data.map(item => {
+        return `${item.firstName} ${item.lastName}`
+    });
+    let ul = document.createElement('ul');
+    ul.innerHTML =
+        `<li>${dataNew[0]}</li>
+        <li>${dataNew[1]}</li>
+        <li>${dataNew[2]}</li>`;
+    document.body.appendChild(ul);
 
-function makeFriendsList(obj) {
-    let result = obj.map(item => {return `${item.firstName} ${item.lastName}`});
-    console.log(result);
 }
 
 makeFriendsList(friends);
+
+
+
+/*
+let result = obj.map(item => {
+    return `${item.firstName} ${item.lastName}`
+});*/
