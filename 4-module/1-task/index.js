37ed6f7a4e4c442adc6ab452caa18d22e25bb719
Yulya Vorontsova <secondnumber01@gmail.com>
2019-06-14 10:28:18 +0300

    added 2, 3 module

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -5,5 +5,25 @@
  * @param {Friend[]} friends
  * @return {HTMLUListElement}
  */
-function makeFriendsList (friends) {
+
+let friends = [
+    {
+        firstName: 'Artsiom',
+        lastName: 'Mezin'
+    },
+    {
+        firstName: 'Ilia',
+        lastName: 'Kantor'
+    },
+    {
+        firstName: 'Christopher',
+        lastName: 'Michael'
+    }
+];
+
+function makeFriendsList(obj) {
+    let result = obj.map(item => {return `${item.firstName} ${item.lastName}`});
+    console.log(result);
 }
+
+makeFriendsList(friends);
