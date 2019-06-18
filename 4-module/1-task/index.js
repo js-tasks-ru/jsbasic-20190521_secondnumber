913e11cd08b6b7ec3c8407e98ecd3adc5635788c
Yulya Vorontsova <secondnumber01@gmail.com>
2019-06-18 12:33:25 +0300

    Решена задача на создание списка друзей

---
+++ 4-module/1-task/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+
+function makeFriendsList(data) {
+
+    const ul = document.createElement('ul');
+
+    const dataNew = data.map(item => {
+        return `${item.firstName} ${item.lastName}`
+    });
+
+    for (let i = 0; i < dataNew.length; i++) {
+
+    const li = document.createElement('li');
+        li.innerHTML = `${dataNew[i]}`;
+        ul.appendChild(li);
+    }
+
+return ul;
+
+}
+
+
